4e925acb2c21c49759888cfbd10e5108876ce235
Stephen Kerr <kerrstephen17@gmail.com>
2025-02-03 22:11:09 +0000

    imported the iris dataset to my test1 file and updated the task md

--- test1.py
+++ test1.py
@@ -1,8 +1,37 @@
+#Stephen Kerr
+# Testing the iris dataset 
 
+# Machine Learning library that contains example datasets.
 
+import sklearn as skl 
 
-import numpy as np
-import pandas as pd
-import sklearn.datasets 
+# load the dataset.
+data = skl.datasets.load_iris()
 
-from ._base import (load_iris)
+# Look at the data
+##print(data)
+
+# look at the data keys 
+##print(data.keys())
+
+# look at the type of the data
+##print(type(data))
+
+# Look at the data column from the data 
+##print(data['data'])
+
+# Look at the data shape
+##print(data['data'].shape)
+
+# the target
+##print(data['target'])
+
+# target shape
+##print(data['target'].shape)
+
+# target names 
+##print(data['target_names'])
+
+# feature names
+print(data['feature_names'])
+
